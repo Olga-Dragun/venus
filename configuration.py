4ad16ce56138ec87d9f7767daf4071c399de5b1e
Olga-Dragun <o.dragun@inbox.ru>
2025-06-01 22:40:47 +0300

    diplomnii_proekt

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://9518cca3-4174-4792-9a5d-6e3afd203c3a.serverhub.praktikum-services.ru/"
+
+CREATE_ORDER_PATH = "/api/v1/orders"
+
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Ольга",
+    "lastName": "Драгун",
+    "address": "Красная площадь, 1",
+    "metroStation": 4,
+    "phone": "+79215556633",
+    "rentTime": 5,
+    "deliveryDate": "2025-06-06",
+    "comment": "Привет",
+    "color": [
+        "BLACK"
+    ] 
+}
